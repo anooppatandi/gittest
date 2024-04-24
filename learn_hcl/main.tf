e978b3a9e4053256b33e35c1ec46f152b461af7e
anooppatandi <72046524+anooppatandi@users.noreply.github.com>
2024-04-24 13:06:16 -0500

    working ec2

---
+++ learn_hcl/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "ubuntuvm" {
+  ami           = "ami-080e1f13689e07408"
+  instance_type = "t2.micro"
+  key_name      = "CICDtest"
+
+  tags = {
+    Name = "testubuntuVM"
+  }
+}
+
